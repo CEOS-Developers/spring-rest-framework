347183d7d4c4bf7b1bd846fec72f69b8198dd4fb
mrkwon <erased11@gmail.com>
2019-10-03 22:52:44 +0900

    init project

---
+++ src/main/java/dev/ceos/springrestframework/SpringrestframeworkApplication.java
@@ -0,0 +1,13 @@
+package dev.ceos.springrestframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestframeworkApplication.class, args);
+	}
+
+}
